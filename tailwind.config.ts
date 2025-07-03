a0187c4ccc60d41f6d1668d1c552342db3e865f9
Kaleb_Cardona <kalebcard@gmail.com>
2025-07-02 22:24:53 -0500

    Agrega la fuente 'Inter' en tailwind.config.ts y actualiza la clase de fuente en globals.css para utilizarla, mejorando la tipografía de la aplicación. Se aplica la clase 'font-sans' y se ajustan propiedades de texto para optimizar la legibilidad y estética general.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,7 +97,10 @@ const config: Config = {
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
   			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
+  		},
+  		fontFamily: {
+  			sans: ['Inter', 'Arial', 'Helvetica', 'sans-serif'],
+  		},
   	}
   },
   plugins: [require("tailwindcss-animate")],
