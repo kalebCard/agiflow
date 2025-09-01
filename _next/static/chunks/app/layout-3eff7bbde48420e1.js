194a833f41cc5e7c59b1120c6193345f2b2128c1
Kaleb_Cardona <kalebcard@gmail.com>
2025-09-01 15:57:27 -0500

    Updates

---
+++ _next/static/chunks/app/layout-3eff7bbde48420e1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1060:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_e8ce0c"}},4326:()=>{},5380:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1060,23)),Promise.resolve().then(l.t.bind(l,4326,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[770,841,476,571,358],()=>s(5380)),_N_E=e.O()}]);
